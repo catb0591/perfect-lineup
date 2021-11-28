9ab85ce886e6fe602b0b132f067b3fd4c6cacfb3
Cat <catb0591@gmail.com>
2021-11-28 02:19:59 -0500

    Refactored code

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const validateLineup = (lineup) => {
   if (lineup.length !== 9) { return false }
-  if (lineup.filter(playerPosition).length - positionOptions.length !== 2) { return false }// refractor to does not equal 7
+  if (lineup.map(playerPosition).filter(truthyValue => truthyValue === false).length !== 0) { return false }
   if (lineup.reduce(countOF, 0) !== 3) { return false }
-  if (lineup.reduce(teamIDCount, []).filter(num => num > 2).length > 0) { return false }
-  if (lineup.reduce(gameIDCount, []).filter(num => num > 3).length > 0) { return false }
+  if (lineup.reduce(teamIDCount, []).filter(count => count > 2).length > 0) { return false }
+  if (lineup.reduce(gameIDCount, []).filter(count => count > 3).length > 0) { return false }
   if (lineup.reduce(salaryTotal, 0) > 45000) { return false }
 
   return true
 }
 
-let positionOptions = []
+const positionOptions = []
 
 const playerPosition = (player) => {
+  if (player.position === 'OF') { return true }
   if (positionOptions.indexOf(player.position) < 0) {
     positionOptions.push(player.position)
+
+    return true
   }
 
-  return positionOptions.length < 8
+  return false
 }
 
 const countOF = (acc, player) => {
@@ -33,8 +36,6 @@ const gameIDCount = (acc, player) => {
   return acc[player.gameId] ? ++acc[player.gameId] : acc[player.gameId] = 1, acc
 }
 
-const salaryTotal = (acc, player) => {
-  return acc + player.salary
-}
+const salaryTotal = (acc, player) => acc + player.salary
 
 module.exports = validateLineup
