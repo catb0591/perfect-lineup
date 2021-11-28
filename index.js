a71cceeb53f9b6611fed733716fdcbba19004981
Cat <catb0591@gmail.com>
2021-11-28 01:06:37 -0500

    refractor counter functions

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const validateLineup = (lineup) => {
   if (lineup.length !== 9) { return false }
   if (lineup.filter(playerPosition).length - positionOptions.length !== 2) { return false }// refractor to does not equal 7
   if (lineup.reduce(countOF, 0) !== 3) { return false }
-  if (lineup.reduce((acc, curr) => {
-    return acc[curr.teamId] ? ++acc[curr.teamId] : acc[curr.teamId] = 1, acc
-  }, []).filter(num => num > 2).length > 0) { return false }
-  if (lineup.reduce((acc, curr) => {
-    return acc[curr.gameId] ? ++acc[curr.gameId] : acc[curr.gameId] = 1, acc
-  }, []).filter(num => num > 3).length > 0) { return false }
+  if (lineup.reduce(teamIDCount, []).filter(num => num > 2).length > 0) { return false }
+  if (lineup.reduce(gameIDCount, []).filter(num => num > 3).length > 0) { return false }
 
   return true
 }
@@ -26,6 +22,14 @@ const countOF = (acc, player) => {
   if (player.position === 'OF') { return acc + 1 }
 
   return acc
+}
+
+const teamIDCount = (acc, player) => {
+  return acc[player.teamId] ? ++acc[player.teamId] : acc[player.teamId] = 1, acc
+}
+
+const gameIDCount = (acc, player) => {
+  return acc[player.gameId] ? ++acc[player.gameId] : acc[player.gameId] = 1, acc
 }
 
 module.exports = validateLineup
