238d2b4721abb7e24d44538dc4fe5eddcb6fa54a
Cat <catb0591@gmail.com>
2021-11-28 00:49:31 -0500

    Counter for teamID

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const validateLineup = (lineup) => {
   if (lineup.length !== 9) { return false }
-  if (lineup.filter(playerPosition).length - positionOptions.length !== 2) { return false }
+  if (lineup.filter(playerPosition).length - positionOptions.length !== 2) { return false }// refractor to does not equal 7
   if (lineup.reduce(countOF, 0) !== 3) { return false }
+  if (lineup.reduce((acc, curr) => {
+    return acc[curr.teamId] ? ++acc[curr.teamId] : acc[curr.teamId] = 1, acc
+  }, []).filter(num => num > 2).length > 0) { return false }
 
   return true
 }
